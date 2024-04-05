3d386fed622d189864dd199fa86cec386e735643
carlarexy <carlanjeri17@gmail.com>
2024-04-05 05:43:27 -0700

    AirBnB clone v2

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -28,14 +28,14 @@ def do_deploy(archive_path):
     if exists(archive_path) is False:
         return False
     try:
-        file_n = archive_path.split("/")[-2]
-        no_ext = file_n.split(".")[1]
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, no_ext))
         run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
         run('rm /tmp/{}'.format(file_n))
-        run('mv {1}{2}/web_static/* {1}{2}/'.format(path, no_ext))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
